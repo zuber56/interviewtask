135f5f19e5049ca1fbd7efa203224cc7d3814355
zuberkhan <zuberkhan5688@gmail.com>
2021-03-06 13:20:16 +0530

    complet code

--- api/src/router/user.js
+++ api/src/router/user.js
@@ -1,89 +1,89 @@
 const express = require('express')
-const User =require('../models/user')
+const User = require('../models/user')
 const router = new express.Router()
 const mongoose = require("mongoose");
 
 //------------------------>
 //       data insert
 //------------------------>
-router.post('/adduser',(req,res)=>{
+router.post('/adduser', (req, res) => {
     try {
         const { fname, lname, email, role } = req.body;
-        const users =new User({
+        const users = new User({
             fname,
             lname,
             email,
             role,
         });
         users.save()
-        .then((data)=>{
-            return res.status(200).json({
-                message:"add",
-                data:data
+            .then((data) => {
+                return res.status(200).json({
+                    message: "add",
+                    data: data
+                })
+            }).catch((e) => {
+                console.log("error:", e);
             })
-        }).catch((e)=>{
-            console.log("error:", e);
-        })
-     
-        
+
     } catch (error) {
         console.log("data ont founds")
     }
 })
-router.post('/addusdder',async(req,res)=>{
+router.post('/addusdder', async (req, res) => {
     try {
-     const { fname, lname, email, role } = req.body;
+        const { fname, lname, email, role } = req.body;
         if (!fname || !lname || !email || !role) {
             return res.status(422).json({
                 error: "please add all field",
             });
         }
-      await User.findOne({ email: email })
+        await User.findOne({ email: email })
             .then((data) => {
-             if (data) {
-                return res.status(401).json({
-                    error: "email address is alerady used",
-                });
-            }
-            else {
-                const users =new User({
-                    fname,
-                    lname,
-                    email,
-                    role,
-                });
-               user
-                    .save()
-                    .then((data) => {
-                    return res.status(200).json({
-                        message: "user add successfuly",
-                        data,
+                if (data) {
+                    return res.status(401).json({
+                        error: "email address is alerady used",
                     });
-                })
-                 .catch((error) => {
-                    console.log("error:", error);
-                });
-            }
-         })
+                }
+                else {
+                    const users = new User({
+                        fname,
+                        lname,
+                        email,
+                        role,
+                    });
+                    user
+                        .save()
+                        .then((data) => {
+                            return res.status(200).json({
+                                message: "user add successfuly",
+                                data,
+                            });
+                        })
+                        .catch((error) => {
+                            console.log("error:", error);
+                        });
+                }
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(401).json({
             error: "somethinmg went wrong",
         });
-    }})
+    }
+})
 //--------------------------->
 // update the user only one
 //--------------------------->
-router.patch('/updateuser/:id', async(req,res)=>{
-    
+router.patch('/updateuser/:id', async (req, res) => {
+
     try {
         const id = req.params.id;
-       
+
         const { fname, lname, email, role } = req.body;
-        
+
         if (!fname || !lname || !email || !role) {
             return res.status(401).json({
                 error: "please add all ddddsssfield",
@@ -97,14 +97,14 @@ router.patch('/updateuser/:id', async(req,res)=>{
         };
         User.findByIdAndUpdate(id, { $set: updatedata })
             .then((data) => {
-            return res.status(200).json({
-                message: "data updated successfuly",
-                data
-            });
-        })
+                return res.status(200).json({
+                    message: "data updated successfuly",
+                    data
+                });
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(400).json({
@@ -116,7 +116,7 @@ router.patch('/updateuser/:id', async(req,res)=>{
 // delete the user only one
 //--------------------------->
 router.delete('/userdelete/:id', async (req, res) => {
-    const id=req.params.id
+    const id = req.params.id
     try {
         const user = await User.findByIdAndDelete(id)
         if (!user) {
@@ -130,14 +130,14 @@ router.delete('/userdelete/:id', async (req, res) => {
 //--------------------------->
 //read the data only one user
 //--------------------------->
-router.get('/user/:id',async(req,res)=>{
-    const _id=req.params.id
-   try {
-        const user= await User.findById(_id) 
-        if(!user){
-                     return res.status(404).send()
-                 } 
-                 res.send(user)  
+router.get('/user/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
     } catch (error) {
         res.status().send(error)
     }
@@ -145,42 +145,52 @@ router.get('/user/:id',async(req,res)=>{
 //--------------------------->
 //user Search
 //--------------------------->
-router.get('/search/:fname',async(req,res)=>{
+router.get('/search/:fname', async (req, res) => {
+
     try {
-        const fname = req.params.fname;
-        User.find({ fname: fname })
+        const searchFieild = req.params.fname;
+        User.find({ fname: { $regex: searchFieild, $options: '$i' } })
             .then((data) => {
-            return res.status(200).json({
-                data,
-            });
-        })
+                return res.status(200).json({
+                    data,
+                });
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(400).json({
             error: "something went wrong",
         });
     }
+
+    // const searchFieild = req.params.fname;
+    // //console.log(searchFieild)
+    // User.find({ fname: { $regex: searchFieild, $options: '$i' } })
+    //     .then(data => {
+    //         res.send(data);
+    //     })
+
+
 })
 
 
 //--------------------------->
 //role Search 
 //--------------------------->
-router.get('/searchrole/:role',async(req,res)=>{
+router.get('/searchrole/:role', async (req, res) => {
     try {
         const role = req.params.role;
         User.find({ role: role })
             .then((data) => {
-            return res.status(200).json({
-                data,
-            });
-        })
+                return res.status(200).json({
+                    data,
+                });
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(400).json({
@@ -191,42 +201,52 @@ router.get('/searchrole/:role',async(req,res)=>{
 //--------------------------->
 //user Search
 //--------------------------->
-router.get('/searchdata/:fname',async(req,res)=>{
+router.get('/searchdata/:fname', async (req, res) => {
+
     try {
-        const fname = req.params.fname;
-        User.find({ fname: fname })
+        const searchFieild = req.params.fname;
+        User.find({ fname: { $regex: searchFieild, $options: '$i' } })
             .then((data) => {
-            return res.status(200).json({
-                data,
-            });
-        })
+                return res.status(200).json({
+                    data,
+                });
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(400).json({
             error: "something went wrong",
         });
     }
+
+
+    // const searchFieild = req.params.fname;
+    // //console.log(searchFieild)
+    // User.find({ fname: { $regex: searchFieild, $options: '$i' } })
+    //     .then(data => {
+    //         res.send(data);
+    //     })
+
 })
 
 
 //--------------------------->
 //role Search Data 
 //--------------------------->
-router.get('/searchroledata/:role',async(req,res)=>{
+router.get('/searchroledata/:role', async (req, res) => {
     try {
         const role = req.params.role;
         User.find({ role: role })
             .then((data) => {
-            return res.status(200).json({
-                data,
-            });
-        })
+                return res.status(200).json({
+                    data,
+                });
+            })
             .catch((error) => {
-            console.log("error:", error);
-        });
+                console.log("error:", error);
+            });
     }
     catch (error) {
         return res.status(400).json({
@@ -238,20 +258,46 @@ router.get('/searchroledata/:role',async(req,res)=>{
 //--------------------------->
 //read all user
 //--------------------------->
-router.get('/userall',async (req,res)=>{
+router.get('/userall', async (req, res) => {
     try {
         await User.find()
-        .then(data=>{
-            res.send({
-                status:400,
-                data
-            }) 
-        })
+            .then(data => {
+                res.send({
+                    status: 400,
+                    data
+                })
+            })
     } catch (error) {
         res.send({
-            status:404,
-            error:error
-        })   
+            status: 404,
+            error: error
+        })
     }
+})
+
+router.get('/user/search/:fname', async (req, res) => {
+    try {
+        var response = [];
+        const fname = req.params.fname
+        if (req.params.fname) {
+            response = await User.find({ fname: fname });
+            res.send(response)
+        }
+        return response
+    } catch (error) {
+        res.send({
+            status: 404,
+            erorr: error
+        })
+    }
+})
+router.get('/searching', (req, res) => {
+
+    const searchFieild = req.body.fname;
+    //console.log(searchFieild)
+    User.find({ fname: { $regex: searchFieild, $options: '$i' } })
+        .then(data => {
+            res.send(data);
+        })
 })
 module.exports = router
